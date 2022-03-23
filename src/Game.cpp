f5cd38c7cf1750d21e583a4039db9339af9a90f2
pedro-git-projects <pedro.generic.email@gmail.com>
2022-03-23 10:26:51 -0300

    initializing & rendering w SDL|simple game loop

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,9 +1,14 @@
 #include "Game.h"
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_video.h>
+#include <cstddef>
 #include <iostream>
-#include <ostream>
+#include <SDL2/SDL.h>
 
 /* Constructor*/
 Game::Game() {
+	isRunning = false;
 	std::cout << "Game constructor invoked" << std::endl;
 }
 
@@ -11,14 +16,68 @@ Game::~Game() {
 	std::cout << "Game destructor invoked" << std::endl;
 }
 
-void Game::Initialize(){}
+void Game::Initialize() {
+	/* Initialize SDL */
+	if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::cerr << "Failed to initialize SDL" << std::endl;
+		return;
+	}
+	/* Populate window pointer */
+	window = SDL_CreateWindow(
+			NULL, 
+			SDL_WINDOWPOS_CENTERED, 
+			SDL_WINDOWPOS_CENTERED, 
+			800, 
+			600, 
+			SDL_WINDOW_BORDERLESS
+		);	
+	if(!window) {
+		std::cerr << "Failed to create window" << std::endl;
+	}
+	/* Populate renderer pointer*/
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if(!renderer) {
+		std::cerr << "Failed to create renderer" << std::endl;
+		return;
+	}
+	/* Nothing failed, engine is running */
+	isRunning = true;
+}
 
-void Game::Run(){}
+void Game::Run() { 
+	while (isRunning) {
+		ProcessInput();
+		Update();
+		Render();
+	}
+}
 
-void Game::ProcessInput() {}
+void Game::ProcessInput() {
+	SDL_Event sdlEvent;
+	while (SDL_PollEvent(&sdlEvent)) {
+		switch (sdlEvent.type) {
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+			case SDL_KEYDOWN:
+				if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+					isRunning = false;
+				}
+				break;
+		}
+	}
+}
 
 void Game::Update(){}
 
-void Game::Render(){}
+void Game::Render() {
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+}
 
-void Game::Destroy(){}
+void Game::Destroy() {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
